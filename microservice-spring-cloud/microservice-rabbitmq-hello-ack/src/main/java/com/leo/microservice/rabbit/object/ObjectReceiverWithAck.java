78b6f3a0ba7508218619b22e12849c194cda232e
a5535772 <214529283@qq.com>
2018-04-18 18:18:00 +0800

    新增 ack 项目

---
+++ microservice-spring-cloud/microservice-rabbitmq-hello-ack/src/main/java/com/leo/microservice/rabbit/object/ObjectReceiverWithAck.java
@@ -0,0 +1,48 @@
+package com.leo.microservice.rabbit.object;
+
+import java.io.IOException;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import com.leo.microservice.model.User;
+import com.rabbitmq.client.Channel;
+
+@Component
+@RabbitListener(queues = "object")
+public class ObjectReceiverWithAck {
+	
+	@Autowired
+	Channel channel;
+	
+
+	@RabbitHandler
+	public void process(User user, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag, Channel channel) {
+
+		try {
+			// do my job
+			doMyJob(user);
+			//消息的标识，false只确认当前一个消息收到，true确认所有consumer获得的消息
+			channel.basicAck(deliveryTag, false);
+		} catch (Exception e) {
+//			e.printStackTrace();
+			try {
+				//requeue 值为 true 表示该消息重新放回队列头，值为 false 表示放弃这条消息。
+				channel.basicReject(deliveryTag, true);
+				
+//				channel.basicNack(deliveryTag, false, true);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	private void doMyJob(User user) throws Exception {
+		System.out.println("Receiver object : " + user);
+		throw new RuntimeException();
+	}
+}
---
+++ microservice-spring-cloud/microservice-rabbitmq-hello-ack/src/main/java/com/leo/microservice/rabbit/topic/TopicReceiver.java
@@ -0,0 +1,16 @@
+package com.leo.microservice.rabbit.topic;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "topic.message")
+public class TopicReceiver {
+
+    @RabbitHandler
+    public void process(String message) {
+        System.out.println("Topic Receiver1  : " + message);
+    }
+
+}
